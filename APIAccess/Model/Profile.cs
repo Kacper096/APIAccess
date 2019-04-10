4aa0e4642293f9e7ecb56e78e38f3b28bd98fa1d
Kacper Matuszek <kacpermatuszek2@gmail.com>
2019-04-10 21:29:41 +0200

    ViewModel and Model about league data

---
+++ APIAccess/Model/Profile.cs
@@ -0,0 +1,49 @@
+﻿using APIAccess.Api;
+using APIAccess.Api.Model;
+using APIAccess.Api.Model.Search;
+using System.Linq;
+
+
+namespace APIAccess.Model
+{
+    public class Profile
+    {
+        public Profile()
+        {
+            var position = Profile.GetLeaguePosition(Access.Summoner);
+            SummonerName = Access.Summoner.Name;
+            Icon = Access.Summoner.PUUID;
+            Level = Access.Summoner.SummonerLevel;
+            Tier = position.Tier;
+            Rank = position.Rank;
+            Emblem = string.Format("/Images/Emblems/Emblem_{0}.png", Tier);
+            Wins = position.Wins;
+            Losses = position.Losses;
+        }
+
+        #region Data Properties
+        public string SummonerName { get; }
+        public string Icon { get; }
+        public long Level { get; }
+        public string Tier { get;  }
+        public string Rank { get; }
+        public string Emblem { get; }
+        public int Wins { get; }
+        public int Losses { get; }
+        #endregion
+
+        /// <summary>
+        /// Get the League Position.
+        /// </summary>
+        /// <param name="summoner"></param>
+        /// <returns></returns>
+        private static LeaguePositionDTO GetLeaguePosition(SummonerDTO summoner)
+        {
+            LeagueV4 league = new LeagueV4(Access.Region);
+
+            var position = league.GetPosition(summoner.Id).Where(p => p.QueueType.Contains("RANKED_SOLO_5x5")).FirstOrDefault();
+
+            return position ?? new LeaguePositionDTO();
+        }
+    }
+}
---
+++ APIAccess/ViewModel/ProfileViewModel.cs
@@ -0,0 +1,27 @@
+﻿using APIAccess.Api.Model.Search;
+using APIAccess.Model;
+using Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIAccess.ViewModel
+{
+    public class ProfileViewModel
+    {
+        private Profile _profile { get; }
+
+        public Profile Profile
+        {
+            get => _profile;
+            set { }
+        }
+
+        public ProfileViewModel()
+        {
+            _profile = new Profile();
+        }
+    }
+}
