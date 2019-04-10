8baa944334ae3d688384916bf327e9e8b4e70372
Kacper Matuszek <kacpermatuszek2@gmail.com>
2019-04-10 21:33:34 +0200

    New DTO class and i renamed namespace of SummonerDTO

---
+++ APIAccess/Model/LeaguePositionDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIAccess.Model
+{
+    public class LeaguePositionDTO
+    {
+        public string QueueType { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public string Rank { get; set; }
+        public string Tier { get; set; }
+    }
+}
--- APIAccess/Model/SummonerDTO.cs
+++ APIAccess/Model/SummonerDTO.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace APIAccess.Api
+namespace APIAccess.Api.Model
 {
     public class SummonerDTO
     {
