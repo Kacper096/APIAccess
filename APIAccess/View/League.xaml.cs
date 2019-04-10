f644f6b2ab0f71cfdf919c0868b6af07a48d07f3
Kacper Matuszek <kacpermatuszek2@gmail.com>
2019-04-10 21:27:53 +0200

    Window About League in ranked solo 5x5

---
+++ APIAccess/View/League.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+
+namespace APIAccess.View
+{
+    /// <summary>
+    /// Interaction logic for League.xaml
+    /// </summary>
+    public partial class League : Window
+    {
+        public League()
+        {
+            InitializeComponent();
+           // var vm = new ViewModel.ProfileViewModel();
+           // this.DataContext = vm;
+        }
+    }
+}
