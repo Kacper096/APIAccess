7c6850ed048c3dfdf859b8ba082f367900d2167f
Kacper Matuszek <kacpermatuszek2@gmail.com>
2019-04-11 16:00:52 +0200

    Window Config.

---
+++ APIAccess/ViewModel/AppViewModel.cs
@@ -0,0 +1,31 @@
+﻿using APIAccess.View;
+using Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace APIAccess.ViewModel
+{
+    public enum Windows
+    {
+        MainWindow,
+        League
+    }
+    static class AppViewModel
+    {
+        public static void ShowWin(Windows toOpen)
+        {
+            switch(toOpen)
+            {
+                case Windows.League:
+                    var league = new League();
+                    league.Show();
+                    break;
+            }
+        }
+    }
+}
